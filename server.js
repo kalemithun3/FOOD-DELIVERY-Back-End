7af74eb67d64c820c974271b9b20432ee5e3ed60
Megha Kale <mithunkalem2@gmail.com>
2024-10-08 09:30:39 +0530

    added code for add item remove item

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import 'dotenv/config.js';
+import cartRouter from './routes/cartRoute.js';
 
 
 
@@ -21,9 +22,9 @@ connectDB();
 
 // api endpoints
 app.use('/api/food', foodRouter);
-
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
 
 app.get('/', (req, res) => {
     res.send("API  is working");
